ced5597b08b1c81e6de9fab423226c6a784eb084
Martijn Bos <martijnbos@me.com>
2020-07-10 18:53:01 +0200

    First Program

---
+++ elevator.py
@@ -0,0 +1,29 @@
+class Elevator():
+    def __init__(self, floors):
+        self.floors = floors
+        self.position = 0
+        self.door = 'closed'
+        
+    def status(self):
+        print('Floor: ' + str(self.position))
+        print('Door: ' + self.door)
+        
+    def move_to(self, floor_nr):
+        if floor_nr > self.floors:
+            print('This elevator cannot reach this floor')
+        elif self.door != 'closed':
+            print('Close the door please')
+        else:
+            self.position = floor_nr
+            print('Floor: ' + str(self.position))
+            print('Door: ' + self.door)
+        
+    def open_door(self):
+        self.door = 'open'
+        print('Floor: ' + str(self.position))
+        print('Door: ' + self.door)
+    
+    def close_door(self):
+        self.door = 'closed'
+        print('Floor: ' + str(self.position))
+        print('Door: ' + self.door)
